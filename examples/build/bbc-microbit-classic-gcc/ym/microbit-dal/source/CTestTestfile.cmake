d7bad36ade9f1f31605dd39da6698815e9925601
m4-fitzgerald <martyn2.fitzgerald@live.uwe.ac.uk>
2019-03-19 17:45:43 +0000

    fixed untracked files

--- examples/build/bbc-microbit-classic-gcc/ym/microbit-dal/source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/IoT-Projects/challenge-3/examples/yotta_modules/microbit-dal/source
-# Build directory: /home/student/IoT-Projects/challenge-3/examples/build/bbc-microbit-classic-gcc/ym/microbit-dal/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
