4e3f54e811a59f57e5f5839218dd63fe1bbcf4fc
m4-fitzgerald <martyn2.fitzgerald@live.uwe.ac.uk>
2019-03-19 17:37:58 +0000

    Initial commit

---
+++ examples/build/bbc-microbit-classic-gcc/ym/mbed-classic/existing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/IoT-Projects/challenge-3/examples/yotta_modules/mbed-classic
+# Build directory: /home/student/IoT-Projects/challenge-3/examples/build/bbc-microbit-classic-gcc/ym/mbed-classic/existing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
